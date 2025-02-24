81e2f78559f86529687b6074f33bcc1b259590fb
Plastinin Alexander <aleksplast6@gmail.com>
2025-02-24 18:16:04 +0300

    [task1] Rule of five for RedBlackTree     - Implemented rule of five for RedBlackTree

--- task1/include/node.hpp
+++ task1/include/node.hpp
@@ -30,11 +30,29 @@ public:
     // Set color of the node
     void set_color(Color &color) { m_color = color; }
 
+    // Get right child of the node
+    TreeNode *get_right() const { return m_right; }
+    // Get left child of the node
+    TreeNode *get_left() const { return m_left; }
+    // Get parent of the node
+    TreeNode *get_parent() const { return m_parent; }
+    // Set right child of the node
+    void set_right(TreeNode *right) {m_right = right; }
+    // Set left child of the node
+    void set_left(TreeNode *left) { m_left = left; }
+    // Set parent of the node
+    void set_parent(TreeNode *parent) { m_parent = parent; }
+
     // TreeNode constructor
-    TreeNode(const KeyT &key = 0, const TreeNode<KeyT> *right = nullptr,
-            const TreeNode<KeyT> *left = nullptr, const TreeNode<KeyT> parent = nullptr,
+    TreeNode(const KeyT &key = 0, const TreeNode<KeyT> parent = nullptr,
+            const TreeNode<KeyT> *right = nullptr, const TreeNode<KeyT> *left = nullptr,
             Color color = Color::Black)
     : m_key(key), m_right(right), m_left(left), m_parent(parent), m_color(color) {}
-};
+
+    TreeNode(const TreeNode *other) {
+        m_color = other->m_color;
+        m_key = other->m_key;
+    }
+}; // class TreeNode
 
 } // namespace Task1
--- task1/include/tree.hpp
+++ task1/include/tree.hpp
@@ -34,24 +34,98 @@ private:
     void erase_fixup(Node *deleted_node);
 public:
     // Insert value into the RedBlackTree
-    void insert(KeyT value);
+    void insert(KeyT &value);
     // Erase value from the RedBlackTree
-    void erase(KeyT value);
+    void erase(KeyT &value);
     // Join another tree into this
     void join(RedBlackTree &other);
 
     // Default constructor
     RedBlackTree(){};
     // Copy constructor
-    RedBlackTree(const RedBlackTree &rhs);
+    RedBlackTree(const RedBlackTree &rhs) {
+        m_root = new Node(rhs.m_root);
+        m_size = rhs.m_size;
+
+        Node *curr = m_root;
+        Node *rhs_curr = rhs.m_root;
+
+        while (rhs_curr) {
+            if (curr->get_left() == nullptr && rhs_curr->get_left()) {
+                Node *parent = curr;
+                Node *rhs_left = rhs_curr->get_left();
+                Node *curr_left = new Node(rhs_left);
+
+                rhs_curr = rhs_left;
+                curr->set_left(curr_left);
+
+                curr = curr->get_left();
+                curr->set_parent(parent);
+            } else if (curr->get_right() == nullptr && rhs_curr->get_right()) {
+                Node *parent = curr;
+                Node *rhs_right = rhs_curr->get_right();
+                Node *curr_right = new Node(rhs_right);
+
+                rhs_curr = rhs_right;
+                curr->set_right(curr_right);
+
+                curr = curr->get_right();
+                curr->set_parent(parent);
+            } else {
+                if (curr == m_root) {
+                    break;
+                } else {
+                    curr = curr->get_parent();
+                    rhs_curr = rhs_curr->get_parent();
+                }
+            }
+        }
+    }
     // Move constructor
-    RedBlackTree(const RedBlackTree &&rhs);
+    RedBlackTree(RedBlackTree &&rhs) {
+        std::swap(m_root, rhs.m_root);
+        std::swap(m_root, rhs.m_size);
+    }
     // Copy assignment
-    RedBlackTree &operator=(const RedBlackTree &rhs);
+    RedBlackTree &operator=(const RedBlackTree &rhs) {
+        RedBlackTree<KeyT> temp(rhs);
+        *this = std::move(rhs);
+
+        return *this;
+    }
     // Move assignment
-    RedBlackTree &operator=(const RedBlackTree &&rhs);
+    RedBlackTree &operator=(const RedBlackTree &&rhs) {
+        std::swap(m_root, rhs.m_root);
+        std::swap(m_root, rhs.m_size);
+
+        return *this;
+    }
     // Destructor
-    ~RedBlackTree();
-};
+    ~RedBlackTree() {
+        Node *curr_node = m_root;
+
+        while (curr_node) {
+            if (curr_node->get_left()) {
+                curr_node = curr_node->get_left();
+            } else if (curr_node->get_right()){
+                curr_node = curr_node->get_right();
+            } else {
+                if (curr_node == m_root) {
+                    break;
+                } else {
+                    Node *parent = curr_node->get_parent();
+                    if (parent->get_right() == curr_node) {
+                        parent->set_right(nullptr);
+                    } else if (parent->get_left() == curr_node) {
+                        parent->set_left(nullptr);
+                    }
+
+                    delete curr_node;
+                    curr_node = parent;
+                }
+            }
+        }
+    }
+}; // class RedBlackTree
 
 } //namespace Task1
